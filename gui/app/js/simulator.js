573f6dd4cbfab894bf4f01a1944c3ba773224f2b
Chirag Sakhuja <dev.sakhuja@gmail.com>
2017-07-26 15:56:19 -0500

    [gui] Add dynamic rows to memory panel

--- gui/app/js/simulator.js
+++ gui/app/js/simulator.js
@@ -1,5 +1,11 @@
 const lc3interface = require('bindings')('addon');
 
+var numberOfRows = Math.ceil($('#mem-panel').height() / 22);
+var memTable = $('#mem-table > tbody');
+for (var i = 0; i < numberOfRows; i++) {
+    memTable.append('<tr class="mem-addr-' + i + '"><td>Thing</td></tr>');
+}
+
 lc3interface.InitializeSimulator();
 $('#console').empty();
 $('#console').html(lc3interface.GetOutput());
@@ -15,10 +21,10 @@ $('.reg-name').each(function() {
         } else if((val & 0x0002) == 2) {
             cc = 'Z';
         }
-        $("#cc-str").html(cc);
+        $('#cc-str').html(cc);
     } else {
         var hex_val = lc3interface.GetRegValueHex(reg_name);
         var dec_val = lc3interface.GetRegValueDec(reg_name);
-        $('#' + reg_name + '-val').html(hex_val + " (" + dec_val + ")");
+        $('#' + reg_name + '-val').html(hex_val + ' (' + dec_val + ')');
     }
 });
