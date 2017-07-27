26766709ab8cd1b5a3460710afe12b4a19998f52
Chirag Sakhuja <dev.sakhuja@gmail.com>
2017-07-26 23:35:15 -0500

    [gui] Add infinite scrolling and scrollbar to mem

--- gui/app/js/simulator.js
+++ gui/app/js/simulator.js
@@ -1,35 +1,10 @@
 const lc3interface = require('bindings')('addon');
 const sprintf = require('sprintf-js').sprintf;
 
-function generateMemRow(addr)
-{
-    var hex_addr = sprintf('0x%04x', addr);
-    var val = lc3interface.GetMemValue(addr);
-    var bin_val = sprintf('%016b', val);
-    var hex_val = sprintf('0x%04X', val);
-    var line = lc3interface.GetMemLine(addr);
-
-    var html = '<tr class="mem-value" id="' + addr + '">'
-    html += '<td class="mem-bp"></td>';
-    html += '<td class="mem-addr">' + hex_addr + '</td>';
-    html += '<td class="mem-bin">' + bin_val + '</td>';
-    html += '<td class="mem-hex">' + hex_val + '</td>';
-    html += '<td class="mem-line">' + line + '</td>';
-    html += '</tr>'
-
-    return html;
-}
-
 lc3interface.InitializeSimulator();
 $('#console').empty();
 $('#console').html(lc3interface.GetOutput());
 lc3interface.ClearOutputBuffer();
-
-var numberOfRows = Math.ceil($('#mem-panel').height() / $("#r0-val").height());
-var memTable = $('#mem-table > tbody');
-for (var i = 0; i < numberOfRows; i++) {
-    memTable.append(generateMemRow(i + 0x1E));
-}
 
 $('.reg-name').each(function() {
     var reg_name = $(this).html().toLowerCase();
@@ -45,6 +20,7 @@ $('.reg-name').each(function() {
     } else {
         var val = lc3interface.GetRegValue(reg_name);
         var hex_val = sprintf('0x%04X', val);
-        $('#' + reg_name + '-val').html(hex_val + ' (' + val + ')');
+        $('#' + reg_name + '-hex').html(hex_val);
+        $('#' + reg_name + '-dec').html(val);
     }
 });
