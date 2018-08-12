5951ab33cbf764c468cbb29d1ddb7aeb0d84b5f1
Chirag Sakhuja <dev.sakhuja@gmail.com>
2018-08-12 18:52:23 -0500

    [gui] Remove old GUIs

--- frontend/old_gui/app/js/common.js
+++
@@ -1,4 +0,0 @@
-$(".btn").click(function(e) {
-    // Removes focus of the button.
-    $(this).blur();
-});
--- frontend/old_gui/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export npm_config_target=1.7.4
-export npm_config_arg=x64
-export npm_config_target_arch=x64
-export npm_config_disturl=https://atom.io/download/electron
-export npm_config_runtime=electron
-export npm_config_build_from_source=true
-HOME=./.electron-gyp npm install
