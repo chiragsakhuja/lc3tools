4c8f0cb549794e391e7b0f6669d003aec09fb63c
Chirag Sakhuja <dev.sakhuja@gmail.com>
2021-01-08 15:38:24 -0600

    [gui] See full description
    - Add feature to ignore updates
    - Tweak settings panel view

--- src/gui/src/renderer/App.vue
+++ src/gui/src/renderer/App.vue
@@ -7,8 +7,20 @@
         <v-toolbar-title>
           <strong>LC3</strong>Tools
         </v-toolbar-title>
+        <v-tooltip bottom v-if="update_available">
+          <v-btn
+            color="green"
+            icon
+            flat
+            @click="downloadUpdate"
+            slot="activator"
+          >
+            <v-icon>info</v-icon>
+          </v-btn>
+          <span>Update</span>
+        </v-tooltip>
         <v-menu model="settings_menu" :close-on-content-click="false" offset-y>
-          <v-btn flat slot="activator">
+          <v-btn icon flat slot="activator">
             <v-icon>settings</v-icon>
           </v-btn>
           <v-card>
@@ -37,14 +49,17 @@
                   <v-switch @change="saveSettings('privilege')" v-model="settings.ignore_privilege"></v-switch>
                 </v-flex>
               </v-layout>
+              <p class="text-red" v-if="settings.ignore_privilege">May result in inconsistency with the grader.</p>
               <v-layout row>
                 <v-flex grow><h3>Use less strict assembly</h3></v-flex>
                 <v-flex shrink>
                   <v-switch @change="saveSettings('liberal-asm')" v-model="settings.liberal_asm"></v-switch>
                 </v-flex>
               </v-layout>
-              <p class="text-red" v-if="settings.liberal_asm">Might cause issues during grading</p>
-              <h4>Issues? Email chirag.sakhuja@utexas.edu</h4>
+              <p class="text-red" v-if="settings.liberal_asm">May result in inconsistency with the grader.</p>
+              <v-layout row>
+                <v-flex grow><h4>Issues? Email chirag.sakhuja@utexas.edu</h4></v-flex>
+              </v-layout>
             </v-container>
           </v-card>
         </v-menu>
@@ -75,34 +90,57 @@
         persistent
       >
         <v-card>
-          <v-card-title v-if="!download_bar" class="headline">Update available</v-card-title>
+          <v-card-title v-if="!download_bar" class="headline">Update Available</v-card-title>
 
           <v-card-text>
             {{
               download_bar ?
                 ("Downloading at " + (update.download_speed / 1024).toFixed(0) + " KB/s") :
-                "A newer version of LC3Tools is available. Would you like to download the update? (It will be fast, and you really should!)"
+                ("Would you like to update now?")
             }}
             <v-progress-linear v-if="download_bar" v-bind:value="(update.download_transferred / update.download_size) * 100"></v-progress-linear>
           </v-card-text>
 
           <v-card-actions v-if="!download_bar">
             <v-spacer></v-spacer>
-            <v-btn
-              color="green darken-1"
-              flat="flat"
-              @click="update_dialog = false"
-            >
-              Cancel
-            </v-btn>
+            <v-tooltip top>
+              <v-btn
+                icon
+                flat
+                @click="ignoreUpdate"
+                slot="activator"
+              >
+                <v-icon>delete</v-icon>
+              </v-btn>
+              <span>Ignore</span>
+            </v-tooltip>
 
-            <v-btn
-              color="green darken-1"
-              flat="flat"
-              @click="updateConfirmed"
-            >
-              Ok
-            </v-btn>
+            <v-tooltip top>
+              <v-btn
+                color="red darken-1"
+                icon
+                flat
+                @click="update_dialog = false"
+                slot="activator"
+              >
+                <v-icon>thumb_down</v-icon>
+              </v-btn>
+              <span>No</span>
+            </v-tooltip>
+
+            <v-tooltip top>
+              <v-btn
+                color="green darken-1"
+                icon
+                flat
+                @click="updateConfirmed"
+                slot="activator"
+              >
+                <v-icon>thumb_up</v-icon>
+              </v-btn>
+              <span>Yes</span>
+            </v-tooltip>
+
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -129,13 +167,15 @@ export default {
         download_size: 0
       },
       update_dialog: false,
+      update_available: false,
       download_bar: false,
       settings_menu: false,
       settings: {
         theme: "light",
         numbers: "unsigned",
         ignore_privilege: false,
-        liberal_asm: false
+        liberal_asm: false,
+        ignore_update: false
       }
     };
   },
@@ -149,7 +189,8 @@ export default {
     ipcRenderer.on("auto_updater", (event, message, progress) => {
       if(message === "update_available") {
         // Show the settings modal
-        this.update_dialog = true;
+        this.update_dialog = ! this.settings.ignore_update;
+        this.update_available = true;
       }
       if(message === "download_progress") {
         this.update.download_speed = progress.bytesPerSecond;
@@ -162,6 +203,8 @@ export default {
   methods: {
     // Updater
     updateConfirmed: function() {
+      this.settings.ignore_update = false;
+      this.saveSettings('ignore-update');
       this.download_bar = true;
       ipcRenderer.send("auto_updater", "update_confirmed");
     },
@@ -180,6 +223,16 @@ export default {
           });
         }
       });
+    },
+    downloadUpdate() {
+      this.settings.ignore_update = false;
+      this.update_dialog = true;
+      this.saveSettings('ignore-update');
+    },
+    ignoreUpdate: function() {
+      this.settings.ignore_update = true;
+      this.update_dialog = false;
+      this.saveSettings('ignore-update');
     },
     saveSettings: function(setting) {
       this.updateGlobals(setting)
@@ -205,6 +258,8 @@ export default {
       } else if(setting === 'liberal-asm') {
         lc3.SetEnableLiberalAsm(this.settings.liberal_asm)
         this.$store.commit('setLiberalAsm', this.settings.liberal_asm)
+      } else if(setting === 'ignore-update') {
+        this.$store.commit('setIgnoreUpdate', this.settings.ignore_update)
       }
     }
   },
--- src/gui/src/renderer/store/modules/Settings.js
+++ src/gui/src/renderer/store/modules/Settings.js
@@ -2,7 +2,8 @@ const state = {
   theme: "light",
   number_type: "unsigned",
   ignore_privilege: false,
-  liberal_asm: false
+  liberal_asm: false,
+  ignore_update: false
 }
 
 const mutations = {
@@ -17,6 +18,9 @@ const mutations = {
   },
   setLiberalAsm(state, setting) {
     state.liberal_asm = setting
+  },
+  setIgnoreUpdate(state, setting) {
+    state.ignore_update = setting
   }
 }
 
@@ -24,7 +28,8 @@ const getters = {
   theme: state => state.theme,
   number_type: state => state.number_type,
   ignore_privilege: state => state.ignore_privilege,
-  liberal_asm: state => state.liberal_asm
+  liberal_asm: state => state.liberal_asm,
+  ignore_update: state => state.ignore_update
 }
 
 export default {
